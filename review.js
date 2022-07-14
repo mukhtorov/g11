4be211094f3b41d58c402566da7c9f5adfc2df07
mukhtorov <mukhtorov_1994@mail.ru>
2022-07-14 16:39:27 +0500

    review js

---
+++ review.js
@@ -0,0 +1,16 @@
+// Branch
+// master
+// pull
+// merge request MR
+// repository - papka
+
+// link to github
+// 1 git init
+// 2 git remote add origin {LINK}
+
+// When error happens
+// git config --global user.name  "mukhtorov"
+// git config --global user.email "webbrain@gmail.com"
+
+// PUSH
+// 1 git add .
